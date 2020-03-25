ae90a64ea8f7da3b916998f1932aa0fb94978055
Charley Chen <zwchenbj@cn.ibm.com>
2020-03-25 18:51:58 +0800

    Updates

---
+++ precache-manifest.871283aacb57f05cf65fb56c2dd72401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fa1dcddbdf708817c0c",
+    "url": "/header-sidenav-carbon/static/js/runtime~main.0388564c.js"
+  },
+  {
+    "revision": "851ccc177777aab23642",
+    "url": "/header-sidenav-carbon/static/js/main.f5af6539.chunk.js"
+  },
+  {
+    "revision": "cf5468e80fc6edc699bf",
+    "url": "/header-sidenav-carbon/static/js/2.1e8269fb.chunk.js"
+  },
+  {
+    "revision": "851ccc177777aab23642",
+    "url": "/header-sidenav-carbon/static/css/main.83ce4c49.chunk.css"
+  },
+  {
+    "revision": "afec9c63a16f67aed3a1f6d3d5d55300",
+    "url": "/header-sidenav-carbon/index.html"
+  }
+];
